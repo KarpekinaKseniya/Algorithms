a99f06514abcbe8e9e173c5b58ee3cbad73e08a1
KarpekinaKseniya <feliks.cat37@gmail.com>
2022-11-24 10:10:10 +0300

    Add: Solution to find duplicate chars in string

---
+++ src/test/java/org/example/DuplicateCharactersTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class DuplicateCharactersTest {
+
+    private final DuplicateCharacters duplicateChar = new DuplicateCharacters();
+
+    @ParameterizedTest
+    @MethodSource("provideInputAndExpectedValue")
+    void findDuplicateChar(final String word, final char[] expected) {
+        assertThat(duplicateChar.findDuplicateChar(word), is(expected));
+    }
+
+    private static Stream<Arguments> provideInputAndExpectedValue() {
+        //@formatter:off
+        return Stream.of(
+                Arguments.of("Programming", new char[]{'r', 'g', 'm'}),
+                Arguments.of("Java", new char[]{'a'}),
+                Arguments.of("Easy", null)
+        );
+        //@formatter:on
+    }
+}
