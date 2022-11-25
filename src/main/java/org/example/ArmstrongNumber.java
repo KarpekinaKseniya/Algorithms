dba710796c3b6b3df390cefc6b5dd6b538e140bd
KarpekinaKseniya <feliks.cat37@gmail.com>
2022-11-25 14:52:16 +0300

    Add: add simply tasks for future

---
+++ src/main/java/org/example/ArmstrongNumber.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/*
+    Check if a given number is an Armstrong number
+ */
+class ArmstrongNumber {
+
+    public boolean isArmstrongNumber(final int number) {
+        throw new RuntimeException("Not implemented");
+    }
+}
---
+++ src/main/java/org/example/PrimeFactors.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/*
+    Find all prime factors of a given number
+ */
+class PrimeFactors {
+
+    public int[] findAllPrimeFactors(final int number) {
+        throw new RuntimeException("Not implemented");
+    }
+}
