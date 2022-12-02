0a4b7bcfa416534550ad3dbf17b5262727f078e2
KarpekinaKseniya <feliks.cat37@gmail.com>
2022-12-02 12:16:40 +0300

    Add: add solution for find pivot number

---
+++ src/test/java/org/example/PivotNumberTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class PivotNumberTest {
+
+    private final PivotNumber pivotNumber = new PivotNumber();
+
+    @ParameterizedTest
+    @CsvSource(value = {"8:6", "1:1", "4:-1", "1000:-1"}, delimiter = ':')
+    void shouldReturnPivotNumber(final int number, final int expected) {
+        assertThat(pivotNumber.pivotInteger(number), is(expected));
+    }
+}
