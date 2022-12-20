6df115cd18995a77e01301881de091bd409c9d9c
KarpekinaKseniya <feliks.cat37@gmail.com>
2022-12-20 09:56:07 +0300

    Add: solution for keys and rooms

---
+++ src/test/java/org/example/KeysRoomsTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.Collections.emptyList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class KeysRoomsTest {
+
+    private final KeysRooms keysRooms = new KeysRooms();
+
+    @ParameterizedTest
+    @MethodSource("provideRoomsAndExpected")
+    void shouldReturnCanVisitAllRooms(final List<List<Integer>> rooms, final boolean expected) {
+        assertThat(keysRooms.canVisitAllRooms(rooms), is(expected));
+    }
+
+    private static Stream<Arguments> provideRoomsAndExpected() {
+        //@formatter:off
+        return Stream.of(
+                Arguments.of(List.of(List.of(1), List.of(2), List.of(3), emptyList()), true),
+                Arguments.of(List.of(List.of(1,3),List.of(3,0,1),List.of(2), List.of(0)), false),
+                Arguments.of(List.of(List.of(2),emptyList(),List.of(1)), true)
+        );
+        //@formatter:on
+    }
+}
