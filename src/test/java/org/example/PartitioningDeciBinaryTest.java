94a5972483e70f2099caca24f34435c526cd90f9
KarpekinaKseniya <feliks.cat37@gmail.com>
2022-12-28 12:18:15 +0300

    Add: add solution for partitioning into minimum number of deci-binary numbers

---
+++ src/test/java/org/example/PartitioningDeciBinaryTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class PartitioningDeciBinaryTest {
+
+    private final PartitioningDeciBinary partitioning = new PartitioningDeciBinary();
+
+    @ParameterizedTest
+    @CsvSource(value = { "27346209830709182346:9", "32:3", "82734:8", "5:5", "4104:4" }, delimiter = ':')
+    void minPartitions(final String n, final int minNumber) {
+        assertThat(partitioning.minPartitions(n), is(minNumber));
+    }
+}
