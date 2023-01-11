f840d185b6ef5105f18fb93026c4de0efbfb715b
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-01-11 11:09:11 +0300

    Add: add solution for boolean binary tree

---
+++ src/test/java/org/example/nodes/BoolTreeTest.java
@@ -0,0 +1,37 @@
+package org.example.nodes;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class BoolTreeTest {
+
+    private final BoolTree boolTree = new BoolTree();
+
+    @ParameterizedTest
+    @MethodSource("provideTreeNodeAndExpectedValue")
+    void evaluateTree(final TreeNode root, final boolean expected) {
+        assertThat(boolTree.evaluateTree(root), is(expected));
+    }
+
+    private static Stream<Arguments> provideTreeNodeAndExpectedValue() {
+        //@formatter:off
+        return Stream.of(
+                Arguments.of(new TreeNode(2, new TreeNode(1), new TreeNode(3, new TreeNode(0), new TreeNode(1))), true),
+                Arguments.of(new TreeNode(2, new TreeNode(3, new TreeNode(0), new TreeNode(1)), new TreeNode(1)), true),
+                Arguments.of(new TreeNode(3, new TreeNode(1), new TreeNode(0)), false),
+                Arguments.of(new TreeNode(3, new TreeNode(0), new TreeNode(0)), false),
+                Arguments.of(new TreeNode(3, new TreeNode(1), new TreeNode(1)), true),
+                Arguments.of(new TreeNode(2, new TreeNode(1), new TreeNode(0)), true),
+                Arguments.of(new TreeNode(2, new TreeNode(0), new TreeNode(0)), false),
+                Arguments.of(new TreeNode(2, new TreeNode(1), new TreeNode(1)), true),
+                Arguments.of(new TreeNode(3, new TreeNode(0), new TreeNode(1)), false),
+                Arguments.of(new TreeNode(0), false)
+        );
+        //@formatter:on
+    }
+}
