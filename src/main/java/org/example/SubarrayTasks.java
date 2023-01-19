ac95206a69d4d6f9e5bfbf995dd0660dcc477401
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-01-19 10:55:27 +0300

    Add: add solution for find number of subarray sums divisible by k

--- src/main/java/org/example/SubarrayTasks.java
+++ src/main/java/org/example/SubarrayTasks.java
@@ -1,14 +1,17 @@
 package org.example;
 
-/*
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarrayTasks {
+
+    /*
     Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
     A circular array means the end of the array connects to the beginning of the array.
     Formally, the next element of nums[i] is nums[(i + 1) % n] and the previous element of nums[i] is nums[(i - 1 + n) % n].
     A subarray may only include each element of the fixed buffer nums at most once.
     Formally, for a subarray nums[i], nums[i + 1], ..., nums[j], there does not exist i <= k1, k2 <= j with k1 % n == k2 % n.
-*/
-public class CircularSubarray {
-
+    */
     public int maxSubarraySumCircular(final int[] nums) {
         int os = nums[0];
         int min = nums[0];
@@ -26,5 +29,27 @@ public class CircularSubarray {
             return res2;
         }
         return Math.max(os - res, res2);
+    }
+
+    /*
+        Given an integer array nums and an integer k,
+        return the number of non-empty subarrays that have a sum divisible by k.
+        A subarray is a contiguous part of an array.
+    */
+    public int subarrayDivByK(int[] nums, int k) {
+        final Map<Integer, Integer> map = new HashMap<>();
+        int remainder;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0) {
+                nums[i] += nums[i - 1];
+            }
+            remainder = (nums[i] % k + k) % k;
+            map.put(remainder, map.getOrDefault(remainder, 0) + 1);
+        }
+        int count = map.getOrDefault(0, 0);
+        for (int frequency : map.values()) {
+            count += frequency * (frequency - 1) / 2;
+        }
+        return count;
     }
 }
--- src/test/java/org/example/SubarrayTasksTest.java
+++ src/test/java/org/example/SubarrayTasksTest.java
@@ -8,14 +8,20 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class CircularSubarrayTest {
+class SubarrayTasksTest {
 
-    private final CircularSubarray circular = new CircularSubarray();
+    private final SubarrayTasks circular = new SubarrayTasks();
 
     @ParameterizedTest
     @MethodSource("provideNumsAndMaxSum")
     void shouldReturnMaxSubarraySumCircular(final int[] nums, final int sum) {
         assertThat(circular.maxSubarraySumCircular(nums), is(sum));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNumsAndK")
+    void shouldReturnCountOfSubarrayDivByK(final int[] nums, final int k, final int count) {
+        assertThat(circular.subarrayDivByK(nums, k), is(count));
     }
 
     private static Stream<Arguments> provideNumsAndMaxSum() {
@@ -24,6 +30,16 @@ class CircularSubarrayTest {
                 Arguments.of(new int[] { 1, -2, 3, -2 }, 3),
                 Arguments.of(new int[] { 5, -3, 5 }, 10),
                 Arguments.of(new int[] { -3, -2, -3 }, -2)
+        );
+        //@formatter:on
+    }
+
+    private static Stream<Arguments> provideNumsAndK() {
+        //@formatter:off
+        return Stream.of(
+                Arguments.of(new int[] { 4, 5, 0, -2, -3, 1 }, 5, 7),
+                Arguments.of(new int[] { 5 }, 9, 0),
+                Arguments.of(new int[] {3, 23, 11, 7}, 7, 1)
         );
         //@formatter:on
     }
