d9419b24065ba70baa80d612a3b09e646436a3df
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-02-06 10:24:13 +0300

    Add: solution for shuffle array

--- src/main/java/org/example/WorkWithArrays.java
+++ src/main/java/org/example/WorkWithArrays.java
@@ -3,14 +3,14 @@ package org.example;
 import java.util.HashMap;
 import java.util.Map;
 
-/*
+class WorkWithArrays {
+
+    /*
     Given an array nums of size n, return the majority element.
 
     The majority element is the element that appears more than n / 2 times.
     You may assume that the majority element always exists in the array.
-*/
-class WorkWithArrays {
-
+    */
     public int findMajorityElement(final int[] nums) {
         final int appears = nums.length / 2;
         final Map<Integer, Integer> map = new HashMap<>();
@@ -23,5 +23,19 @@ class WorkWithArrays {
                 .map(Map.Entry::getKey)
                 .findFirst().orElseThrow(() -> new RuntimeException("Majority element not found"));
         //@formatter:off
+    }
+
+    /*
+    Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+    Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+    */
+    public int[] shuffle(final int[] nums, final int n) {
+        int[] result = new int[nums.length];
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            result[j++] = nums[i];
+            result[j++] = nums[i + n];
+        }
+        return result;
     }
 }
--- src/test/java/org/example/WorkWithArraysTest.java
+++ src/test/java/org/example/WorkWithArraysTest.java
@@ -1,14 +1,15 @@
 package org.example;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class WorkWithArraysTest {
 
@@ -22,14 +23,33 @@ class WorkWithArraysTest {
 
     @Test
     void shouldThrowExceptionWhenCannotFindMajorityElement() {
-        final int[] wrongNumsCount = new int[] { 5, 5, 5, 5, 1, 1, 1, 1 };
+        final int[] wrongNumsCount = new int[]{5, 5, 5, 5, 1, 1, 1, 1};
         final RuntimeException exception =
                 assertThrows(RuntimeException.class, () -> workWithArrays.findMajorityElement(wrongNumsCount));
         assertThat(exception.getMessage(), is("Majority element not found"));
     }
 
+    @ParameterizedTest
+    @MethodSource("provideXandY")
+    void shouldShuffleArray(final int[] nums, final int n, final int[] expected) {
+        assertThat(workWithArrays.shuffle(nums, n), is(expected));
+    }
+
+    private static Stream<Arguments> provideXandY() {
+        //@formatter:off
+        return Stream.of(
+                Arguments.of(new int[]{2, 5, 1, 3, 4, 7}, 3, new int[]{2, 3, 5, 4, 1, 7}),
+                Arguments.of(new int[]{1, 1, 2, 2}, 2, new int[]{1, 2, 1, 2}),
+                Arguments.of(new int[]{1, 2, 3, 4, 4, 3, 2, 1}, 4, new int[]{1, 4, 2, 3, 3, 2, 4, 1}));
+        //@formatter:on
+    }
+
     private static Stream<Arguments> provideArrayAndExpectedResult() {
-        return Stream.of(Arguments.of(new int[] { 3, 2, 3 }, 3), Arguments.of(new int[] { 2, 2, 1, 1, 1, 2, 2 }, 2),
+        //@formatter:off
+        return Stream.of(
+                Arguments.of(new int[] { 3, 2, 3 }, 3),
+                Arguments.of(new int[] { 2, 2, 1, 1, 1, 2, 2 }, 2),
                 Arguments.of(new int[] { 5, 5, 5, 3, 2, 3, 5, 5, 1, 5, 3, 5, 5 }, 5));
+        //@formatter:on
     }
 }
