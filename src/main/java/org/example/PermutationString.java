8f6783ec92a2a31a8282be446aac631e294ec456
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-02-20 11:12:33 +0300

    Add: add solution for find first uniq char

--- src/main/java/org/example/PermutationString.java
+++ src/main/java/org/example/PermutationString.java
@@ -1,14 +1,15 @@
 package org.example;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
-/*
-    Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
-    In other words, return true if one of s1's permutations is the substring of s2.
-*/
 public class PermutationString {
 
+    /*
+    Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+    In other words, return true if one of s1's permutations is the substring of s2.
+    */
     public boolean checkInclusion(final String s1, final String s2) {
         final Map<Character, Integer> map = new HashMap<>();
         final Map<Character, Integer> newMap = new HashMap<>();
@@ -22,8 +23,26 @@ public class PermutationString {
             }
             if (map.equals(newMap)) {
                 return true;
-            } else newMap.clear();
+            } else
+                newMap.clear();
         }
         return false;
+    }
+
+    // Given a string s, find the first non-repeating character in it and return its index.
+    // If it does not exist, return -1.
+    public int firstUniqChar(final String s) {
+        final Map<Character, Integer> map = new LinkedHashMap<>();
+        for (final char sym : s.toCharArray()) {
+            map.put(sym, map.getOrDefault(sym, 0) + 1);
+        }
+        for (final Map.Entry<Character, Integer> entry : map.entrySet()) {
+            final Character key = entry.getKey();
+            final Integer count = entry.getValue();
+            if (count == 1) {
+                return s.indexOf(key);
+            }
+        }
+        return -1;
     }
 }
--- src/test/java/org/example/PermutationStringTest.java
+++ src/test/java/org/example/PermutationStringTest.java
@@ -1,22 +1,24 @@
 package org.example;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class PermutationStringTest {
 
     private final PermutationString permutationString = new PermutationString();
 
     @ParameterizedTest
-    @CsvSource(value = {
-            "ab:eidbaooo:true",
-            "ab:eidboaoo:false",
-            "bd:adbade:true"
-    }, delimiter = ':')
+    @CsvSource(value = { "ab:eidbaooo:true", "ab:eidboaoo:false", "bd:adbade:true" }, delimiter = ':')
     void shouldCheckInclusion(final String s1, final String s2, final boolean expected) {
         assertThat(permutationString.checkInclusion(s1, s2), is(expected));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = { "correct:1", "loveprogramming:0", "aassffeee:-1" }, delimiter = ':')
+    void shouldReturnIndexOfFirstUniqChar(final String s, final int expected) {
+        assertThat(permutationString.firstUniqChar(s), is(expected));
     }
 }
