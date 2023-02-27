0666a7f8162192b126a80df96506c9f3218ccb7d
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-02-27 09:26:39 +0300

    Add: solution for left and right sum differences

--- src/main/java/org/example/WorkWithArrays.java
+++ src/main/java/org/example/WorkWithArrays.java
@@ -111,4 +111,29 @@ class WorkWithArrays {
         }
         return Math.min(diff, pq.peek() - min);
     }
+
+    /*
+        Given a 0-indexed integer array nums, find a 0-indexed integer array answer where:
+            answer.length == nums.length.
+            answer[i] = |leftSum[i] - rightSum[i]|.
+        Where:
+            leftSum[i] is the sum of elements to the left of the index i in the array nums.
+            If there is no such element, leftSum[i] = 0.
+            rightSum[i] is the sum of elements to the right of the index i in the array nums.
+            If there is no such element, rightSum[i] = 0.
+Return the array answer.
+    */
+    public int[] leftRightDifference(final int[] nums) {
+        int leftSum = 0, rightSum = 0, len = nums.length;
+        for(final int num : nums) {
+            rightSum += num;
+        }
+        for(int i = 0; i < len; i++) {
+            int val = nums[i];
+            rightSum -= val;
+            nums[i] = Math.abs(leftSum - rightSum);
+            leftSum += val;
+        }
+        return nums;
+    }
 }
