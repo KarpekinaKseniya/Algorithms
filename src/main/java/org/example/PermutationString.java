b1a3124a1d3c7934e1c0c8fb6a1780ec23605aee
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-03-02 08:11:23 +0300

    Add: solution for string compression

--- src/main/java/org/example/PermutationString.java
+++ src/main/java/org/example/PermutationString.java
@@ -45,4 +45,36 @@ public class PermutationString {
         }
         return -1;
     }
+
+    /*
+        Given an array of characters chars, compress it using the following algorithm:
+        Begin with an empty string s. For each group of consecutive repeating characters in chars:
+        If the group's length is 1, append the character to s.
+        Otherwise, append the character followed by the group's length.
+        The compressed string s should not be returned separately, but instead,
+        be stored in the input character array chars. Note that group lengths that are 10 or longer
+        will be split into multiple characters in chars.
+        After you are done modifying the input array, return the new length of the array.
+        You must write an algorithm that uses only constant extra space.
+     */
+    public int compress(final char[] chars) {
+        int index = 0;
+        for (int i = 0; i < chars.length; i++) {
+            int count = 1;
+            while (i < chars.length - 1 && chars[i] == chars[i + 1]) {
+                count++;
+                i++;
+            }
+            chars[index] = chars[i];
+            index++;
+            if (count > 1) {
+                for (char ch : Integer.toString(count).toCharArray()) {
+                    chars[index] = ch;
+                    index++;
+                }
+
+            }
+        }
+        return index;
+    }
 }
