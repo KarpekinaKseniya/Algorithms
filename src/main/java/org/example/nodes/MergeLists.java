a8de591375f38b29d44fd088e8a0f4506e18eb6a
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-03-12 09:44:49 +0300

    ADD: solution for merge sorted lists

--- src/main/java/org/example/nodes/MergeLists.java
+++ src/main/java/org/example/nodes/MergeLists.java
@@ -2,14 +2,16 @@ package org.example.nodes;
 
 import org.example.nodes.models.ListNode;
 
-/*
-    You are given the heads of two sorted linked lists list1 and list2.
-    Merge the two lists in a one sorted list.
-    The list should be made by splicing together the nodes of the first two lists.
-    Return the head of the merged linked list.
-*/
+import java.util.PriorityQueue;
+
 public class MergeLists {
 
+    /*
+        You are given the heads of two sorted linked lists list1 and list2.
+        Merge the two lists in a one sorted list.
+        The list should be made by splicing together the nodes of the first two lists.
+        Return the head of the merged linked list.
+    */
     public ListNode mergeTwoLists(final ListNode list1, final ListNode list2) {
         if (list1 == null) {
             return list2;
@@ -24,5 +26,27 @@ public class MergeLists {
             list2.setNext(mergeTwoLists(list1, list2.getNext()));
             return list2;
         }
+    }
+
+    /*
+        You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+        Merge all the linked-lists into one sorted linked-list and return it.
+     */
+    public ListNode mergeKLists(final ListNode[] lists) {
+        final PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (final ListNode list : lists) {
+            ListNode current = list;
+            while (current != null) {
+                queue.add(current.getVal());
+                current = current.getNext();
+            }
+        }
+        ListNode head = new ListNode();
+        ListNode temp = head;
+        while (queue.size() > 0) {
+            temp.setNext(new ListNode(queue.remove()));
+            temp = temp.getNext();
+        }
+        return head.getNext();
     }
 }
