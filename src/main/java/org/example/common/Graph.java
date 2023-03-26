89e1699d15f4a2ca724de2506ca2fc82806a3731
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-03-26 11:14:32 +0300

    ADD: simple solution for find the longest cycle in graph

---
+++ src/main/java/org/example/common/Graph.java
@@ -0,0 +1,50 @@
+package org.example.common;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+    You are given a directed graph of n nodes numbered from 0 to n - 1,
+    where each node has at most one outgoing edge.
+
+    The graph is represented with a given 0-indexed array edges of size n,
+    indicating that there is a directed edge from node i to node edges[i].
+    If there is no outgoing edge from node i, then edges[i] == -1.
+
+    Return the length of the longest cycle in the graph. If no cycle exists, return -1.
+
+    A cycle is a path that starts and ends at the same node.
+
+    Constraints:
+        n == edges.length
+        2 <= n <= 105
+        -1 <= edges[i] < n
+        edges[i] != i
+ */
+public class Graph {
+
+    public int longestCycle(final int[] edges) {
+        boolean[] visited = new boolean[edges.length];
+        int max = -1;
+        for (int i = 0; i < edges.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                int destination = edges[i];
+                int length = 1;
+                Map<Integer, Integer> nodeToDistance = new HashMap<>();
+                nodeToDistance.put(i, 0);
+                while (destination != -1 && !visited[destination]) {
+                    nodeToDistance.put(destination, length);
+                    visited[destination] = true;
+                    destination = edges[destination];
+                    length++;
+                }
+                if (destination != -1 && nodeToDistance.containsKey(destination)) {
+                    max = Math.max(length - nodeToDistance.get(destination), max);
+                }
+            }
+        }
+        return max;
+    }
+}
