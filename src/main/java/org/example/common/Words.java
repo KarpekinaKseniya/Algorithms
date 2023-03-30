d45ef67ce85d570392125f82d433fa9519666b6f
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-03-30 08:30:47 +0300

    ADD: simple solution for scramble string

--- src/main/java/org/example/common/Words.java
+++ src/main/java/org/example/common/Words.java
@@ -40,6 +40,47 @@ public class Words {
     }
 
     /*
+        We can scramble a string s to get a string t using the following algorithm:
+            If the length of the string is 1, stop.
+        If the length of the string is > 1, do the following:
+            Split the string into two non-empty substrings at a random index, i.e.,
+                if the string is s, divide it to x and y where s = x + y.
+            Randomly decide to swap the two substrings or to keep them in the same order. i.e.,
+                after this step, s may become s = x + y or s = y + x.
+            Apply step 1 recursively on each of the two substrings x and y.
+        Given two strings s1 and s2 of the same length, return true if s2 is a scrambled string of s1,
+            otherwise, return false.
+     */
+    public boolean isScramble(final String s1, final String s2) {
+        final int n = s1.length();
+        if (n != s2.length()) {
+            return false;
+        }
+        boolean[][][] dp = new boolean[n + 1][n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[1][i][j] = true;
+                }
+            }
+        }
+        for (int l = 2; l <= n; l++) {
+            for (int i = 0; i <= n - l; i++) {
+                for (int j = 0; j <= n - l; j++) {
+                    for (int k = 1; k < l; k++) {
+                        if ((dp[k][i][j] && dp[l - k][i + k][j + k]) ||
+                                (dp[k][i][j + l - k] && dp[l - k][i + k][j])) {
+                            dp[l][i][j] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return dp[n][0][0];
+    }
+
+    /*
         We define the usage of capitals in a word to be right when one of the following cases holds:
             All letters in this word are capitals, like "USA".
             All letters in this word are not capitals, like "boring".
