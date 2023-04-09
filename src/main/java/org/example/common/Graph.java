56ccd4d4899ca1036a4df07290838cc8e4294d3a
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-04-09 16:55:41 +0300

    ADD: simple solution for find the largest color value in a directed graph

--- src/main/java/org/example/common/Graph.java
+++ src/main/java/org/example/common/Graph.java
@@ -1,10 +1,13 @@
 package org.example.common;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-/*
+public class Graph {
+
+    /*
     You are given a directed graph of n nodes numbered from 0 to n - 1,
     where each node has at most one outgoing edge.
 
@@ -21,9 +24,7 @@ import java.util.Map;
         2 <= n <= 105
         -1 <= edges[i] < n
         edges[i] != i
- */
-public class Graph {
-
+    */
     public int longestCycle(final int[] edges) {
         boolean[] visited = new boolean[edges.length];
         int max = -1;
@@ -46,5 +47,56 @@ public class Graph {
             }
         }
         return max;
+    }
+
+    /*
+        There is a directed graph of n colored nodes and m edges.
+        The nodes are numbered from 0 to n - 1.
+
+        You are given a string colors where colors[i] is a lowercase English letter representing
+        the color of the ith node in this graph (0-indexed). You are also given a 2D array edges
+        where edges[j] = [aj, bj] indicates that there is a directed edge from node aj to node bj.
+
+        A valid path in the graph is a sequence of nodes x1 -> x2 -> x3 -> ... -> xk such that
+        there is a directed edge from xi to xi+1 for every 1 <= i < k. The color value of the path
+        is the number of nodes that are colored the most frequently occurring color along that path.
+
+        Return the largest color value of any valid path in the given graph, or -1 if the graph
+        contains a cycle.
+     */
+    public int largestPathValue(final String colors, final int[][] edges) {
+        final List<List<Integer>> al = new ArrayList<>();
+        final List<int[]> cnt = new ArrayList<>();
+        final int n = colors.length();
+        for (int i = 0; i < n; i++) {
+            al.add(new ArrayList<>());
+            cnt.add(new int[26]);
+        }
+        int[] indegrees = new int[n];
+        for (int[] e : edges) {
+            al.get(e[0]).add(e[1]);
+            indegrees[e[1]]++;
+        }
+        List<Integer> q = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegrees[i] == 0)
+                q.add(i);
+        }
+        int res = 0, processed = 0;
+        while (!q.isEmpty()) {
+            final List<Integer> q1 = new ArrayList<>();
+            for (final int i : q) {
+                processed++;
+                res = Math.max(res, ++cnt.get(i)[colors.charAt(i) - 'a']);
+                for (final int j : al.get(i)) {
+                    for (int k = 0; k < 26; k++)
+                        cnt.get(j)[k] = Math.max(cnt.get(j)[k], cnt.get(i)[k]);
+                    if (--indegrees[j] == 0)
+                        q1.add(j);
+                }
+            }
+            q = q1;
+        }
+        return processed != n ? - 1 : res;
     }
 }
