309247b4ddd4983c8d3e1282c45731af35e1dbdc
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-04-14 07:57:27 +0300

    ADD: simple solution for find palindrome

--- src/main/java/org/example/common/PalindromeSubstring.java
+++ src/main/java/org/example/common/PalindromeSubstring.java
@@ -5,11 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/*
-    Given a string s, return the longest palindromic substring in s.
-*/
 public class PalindromeSubstring {
 
+    /*
+        Given a string s, return the longest palindromic substring in s.
+    */
     public String longestPalindrome(final String s) {
         final Map<Character, List<Integer>> charMap = new HashMap<>();
         String longestPalindrome = "";
@@ -34,5 +34,26 @@ public class PalindromeSubstring {
             }
         }
         return longestPalindrome.isEmpty() ? s.charAt(0) + "" : longestPalindrome;
+    }
+
+    /*
+        Given a string s, find the longest palindromic subsequence's length in s.
+
+        A subsequence is a sequence that can be derived from another sequence by deleting some
+        or no elements without changing the order of the remaining elements.
+    */
+    public int longestPalindromeSubseq(final String s) {
+        final char[] c = s.toCharArray();
+        int[] dp = new int[c.length];
+        for (int j = 0; j < dp.length; j++) {
+            dp[j] = 1;
+            int topRight = 0;
+            for (int i = j - 1; i >= 0; i--) {
+                int temp = dp[i];
+                dp[i] = c[i] == c[j] ? 2 + topRight : Math.max(dp[i], dp[i + 1]);
+                topRight = temp;
+            }
+        }
+        return dp[0];
     }
 }
