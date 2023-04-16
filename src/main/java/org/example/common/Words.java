feb04091e48b6e6f921d39ddd58aba556fe2e1e9
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-04-16 15:24:29 +0300

    ADD: simple solution for find ways

--- src/main/java/org/example/common/Words.java
+++ src/main/java/org/example/common/Words.java
@@ -234,6 +234,46 @@ public class Words {
     }
 
     /*
+        You are given a list of strings of the same length words and a string target.
+
+        Your task is to form target using the given words under the following rules:
+            target should be formed from left to right.
+            To form the ith character (0-indexed) of target, you can choose the kth character of
+            the jth string in words if target[i] = words[j][k].
+            Once you use the kth character of the jth string of words, you can no longer use
+            the xth character of any string in words where x <= k. In other words, all characters
+            to the left of or at index k become unusuable for every string.
+            Repeat the process until you form the string target.
+            Notice that you can use multiple characters from the same string in words provided
+            the conditions above are met.
+
+        Return the number of ways to form target from words. Since the answer may be too large,
+        return it modulo 109 + 7.
+    */
+    public int numWays(final String[] words, final String target) {
+        final int mod = 1000000007;
+        final int n = words[0].length();
+        int[][] freq = new int[n][26];
+        for (final String word : words) {
+            for (int j = 0; j < n; j++) {
+                freq[j][word.charAt(j) - 'a']++;
+            }
+        }
+        int[][] dp = new int[n + 1][target.length() + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= target.length(); j++) {
+                final int charCount = freq[i - 1][target.charAt(j - 1) - 'a'];
+                dp[i][j] = (dp[i - 1][j] + (int) ((long) charCount * dp[i - 1][j - 1] % mod)) % mod;
+            }
+        }
+
+        return dp[n][target.length()];
+    }
+
+    /*
         Given an array of strings words (without duplicates), return all the concatenated words in the given list of words.
         A concatenated word is defined as a string that is composed entirely of at least two shorter words in the given array.
     */
