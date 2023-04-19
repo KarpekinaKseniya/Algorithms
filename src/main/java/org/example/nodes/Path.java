4d6945f78be637604bed724a429f1278a7458bd1
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-04-19 10:33:15 +0300

    ADD: simple solution for find the longest ZigZag path in binary tree

--- src/main/java/org/example/nodes/Path.java
+++ src/main/java/org/example/nodes/Path.java
@@ -2,15 +2,15 @@ package org.example.nodes;
 
 import org.example.nodes.models.TreeNode;
 
-/*
-    Given the root of a binary tree and an integer targetSum,
-    return true if the tree has a root-to-leaf path such that adding up all
-    the values along the path equals targetSum.
-
-    A leaf is a node with no children.
-*/
 public class Path {
 
+    /*
+        Given the root of a binary tree and an integer targetSum,
+        return true if the tree has a root-to-leaf path such that adding up all
+        the values along the path equals targetSum.
+
+        A leaf is a node with no children.
+    */
     public boolean hasPathSum(final TreeNode root, final int targetSum) {
         if (root == null) {
             return false;
@@ -22,5 +22,40 @@ public class Path {
 
         return hasPathSum(root.getLeft(), targetSum - root.getVal()) ||
                 hasPathSum(root.getRight(), targetSum - root.getVal());
+    }
+
+    /*
+        You are given the root of a binary tree.
+
+        A ZigZag path for a binary tree is defined as follow:
+            Choose any node in the binary tree and a direction (right or left).
+            If the current direction is right, move to the right child of the current node;
+                otherwise, move to the left child.
+            Change the direction from right to left or from left to right.
+            Repeat the second and third steps until you can't move in the tree.
+            Zigzag length is defined as the number of nodes visited - 1. (A single node has a length of 0).
+
+        Return the longest ZigZag path contained in that tree.
+    */
+    public int longestZigZag(final TreeNode root) {
+        int A = process(root.getLeft(), true, 0);
+        int B = process(root.getRight(), false, 0);
+        return Math.max(A, B);
+    }
+
+    private int process(final TreeNode root, final boolean direct, final int count) {
+        if (root == null) return count;
+
+        int A = 0, B = 0, C = 0, D = 0;
+        if (direct) {
+            A = process(root.getRight(), false, count + 1);
+            B = process(root.getLeft(), true, 0);
+        } else {
+            C = process(root.getLeft(), true, count + 1);
+            D = process(root.getRight(), false, 0);
+        }
+        A = Math.max(A, B);
+        C = Math.max(C, D);
+        return Math.max(A, C);
     }
 }
