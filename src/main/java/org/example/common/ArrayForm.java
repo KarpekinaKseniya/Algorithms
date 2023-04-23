0768110e9ac13c92fc6270f953f1098a2480ad1e
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-04-23 19:03:59 +0300

    ADD: simple solution for restore the array

--- src/main/java/org/example/common/ArrayForm.java
+++ src/main/java/org/example/common/ArrayForm.java
@@ -4,14 +4,14 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-/*
-    The array-form of an integer num is an array representing its digits in left to right order.
-    For example, for num = 1321, the array form is [1,3,2,1].
-    Given num, the array-form of an integer, and an integer k,
-    return the array-form of the integer num + k.
-*/
 public class ArrayForm {
 
+    /*
+        The array-form of an integer num is an array representing its digits in left to right order.
+        For example, for num = 1321, the array form is [1,3,2,1].
+        Given num, the array-form of an integer, and an integer k,
+        return the array-form of the integer num + k.
+    */
     public List<Integer> addToArrayForm(final int[] num, final int k) {
         final List<Integer> list = new LinkedList<>();
         int added = k;
@@ -38,5 +38,32 @@ public class ArrayForm {
         }
         Collections.reverse(list);
         return list;
+    }
+
+    /*
+        A program was supposed to print an array of integers.
+        The program forgot to print whitespaces and the array is printed as a string of digits s
+        and all we know is that all integers in the array were in the range [1, k] and there are
+        no leading zeros in the array.
+
+        Given the string s and the integer k, return the number of the possible arrays that
+        can be printed as s using the mentioned program. Since the answer may be very large,
+        return it modulo 109 + 7.
+     */
+    public int numberOfArrays(final String s, final int k) {
+        final int mod = 1000_000_007;
+        int[] dp = new int[s.length() + 1];
+        dp[s.length()] = 1;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            long num = 0, sum = 0;
+            for (int j = i; j < s.length(); j++) {
+                num = num * 10 + (s.charAt(j) - '0');
+                if (num == 0 || num > k)
+                    break;
+                sum += dp[j + 1];
+            }
+            dp[i] = (int) (sum % mod);
+        }
+        return dp[0];
     }
 }
