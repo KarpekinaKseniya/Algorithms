5797af36012241197d4f6be10c3aa1431fa06a71
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-05-19 08:26:51 +0300

    ADD: simple solution for graph

--- src/main/java/org/example/common/Graph.java
+++ src/main/java/org/example/common/Graph.java
@@ -1,6 +1,7 @@
 package org.example.common;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -97,6 +98,36 @@ public class Graph {
             }
             q = q1;
         }
-        return processed != n ? - 1 : res;
+        return processed != n ? -1 : res;
+    }
+
+    /*
+        There is an undirected graph with n nodes, where each node is numbered between 0 and n - 1.
+        You are given a 2D array graph, where graph[u] is an array of nodes that node u
+        is adjacent to. More formally, for each v in graph[u], there is an undirected edge
+        between node u and node v. The graph has the following properties:
+            There are no self-edges (graph[u] does not contain u).
+            There are no parallel edges (graph[u] does not contain duplicate values).
+            If v is in graph[u], then u is in graph[v] (the graph is undirected).
+            The graph may not be connected, meaning there may be two nodes u and v such
+                that there is no path between them.
+        A graph is bipartite if the nodes can be partitioned into two independent sets A and B such
+        that every edge in the graph connects a node in set A and a node in set B.
+        Return true if and only if it is bipartite.
+    */
+    public boolean isBipartite(final int[][] graph) {
+        int[] color = new int[graph.length];
+        Arrays.fill(color, -1);
+        for (int i = 0; i < graph.length; i++)
+            if (color[i] == -1 && !dfs(graph, i, color, 0)) return false;
+        return true;
+    }
+
+    private boolean dfs(final int[][] graph, final int u, final int[] color, final int c) {
+        color[u] = c;
+        for (int v : graph[u])
+            if (color[v] == -1 && !dfs(graph, v, color, (c + 1) % 2)) return false;
+            else if (color[v] != (c + 1) % 2) return false;
+        return true;
     }
 }
