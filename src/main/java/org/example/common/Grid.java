f55e98305195aacf48567dac278297642938c34d
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-06-18 10:40:00 +0300

    ADD: simple solution for find number of increase

--- src/main/java/org/example/common/Grid.java
+++ src/main/java/org/example/common/Grid.java
@@ -1,10 +1,13 @@
 package org.example.common;
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Grid {
+
+    private final static int MOD = (int) (1e9 + 7);
 
     /*
         Given an n x n binary matrix grid, return the length of the shortest clear path in the matrix.
@@ -88,5 +91,47 @@ public class Grid {
             }
         }
         return count;
+    }
+
+    /*
+        You are given an m x n integer matrix grid, where you can move from a cell to
+        any adjacent cell in all 4 directions.
+        Return the number of strictly increasing paths in the grid such that you can
+        start from any cell and end at any cell. Since the answer may be very large,
+        return it modulo 10^9 + 7.
+        Two paths are considered different if they do not have exactly the same sequence
+        of visited cells.
+    */
+    public int countPaths(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int[][] dp = new int[n][m];
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
+        }
+        int paths = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                paths = (paths + solve(grid, i, j, -1, dp)) % MOD;
+            }
+        }
+        return paths;
+
+    }
+
+    private int solve(int[][] grid, int i, int j, int prev, int[][] dp) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] <= prev) {
+            return 0;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int left = solve(grid, i, j - 1, grid[i][j], dp);
+        int right = solve(grid, i, j + 1, grid[i][j], dp);
+        int up = solve(grid, i - 1, j, grid[i][j], dp);
+        int down = solve(grid, i + 1, j, grid[i][j], dp);
+
+        return dp[i][j] = (1 + left + right + up + down) % MOD;
     }
 }
