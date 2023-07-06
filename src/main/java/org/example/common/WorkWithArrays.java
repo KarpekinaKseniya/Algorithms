cec92873f532ea85376529e744be1fd7274f39d7
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-07-06 07:35:18 +0300

    ADD: simply solution for min sub array len

--- src/main/java/org/example/common/WorkWithArrays.java
+++ src/main/java/org/example/common/WorkWithArrays.java
@@ -404,4 +404,27 @@ class WorkWithArrays {
     return result;
   }
 
+  /*
+    Given an array of positive integers nums and a positive integer target,
+    return the minimal length of a subarray whose sum is greater than or
+    equal to target. If there is no such subarray, return 0 instead.
+  */
+  public int minSubArrayLen(int target, int[] nums) {
+    int i = 0;
+    int j = 0;
+    int min = Integer.MAX_VALUE;
+    int sum = 0;
+    while(j < nums.length){
+      sum += nums[j];
+      if(sum >= target){
+        while(sum >= target){
+          min = Math.min(min,j - i + 1);
+          sum -= nums[i];
+          i++;
+        }
+      }
+      j++;
+    }
+    return min == Integer.MAX_VALUE ? 0 : min;
+  }
 }
