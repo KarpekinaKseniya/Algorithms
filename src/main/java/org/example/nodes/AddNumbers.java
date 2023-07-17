c4d76732593303871ccaace7c2a3f6632f261bf9
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-07-17 07:28:24 +0300

    ADD: simply solution for add lists node

--- src/main/java/org/example/nodes/AddNumbers.java
+++ src/main/java/org/example/nodes/AddNumbers.java
@@ -2,18 +2,60 @@ package org.example.nodes;
 
 import org.example.nodes.models.ListNode;
 
-/*
-    You are given two non-empty linked lists representing two non-negative integers.
-    The digits are stored in reverse order, and each of their nodes contains a single digit.
-    Add the two numbers and return the sum as a linked list.
+import java.util.Stack;
 
-    You may assume the two numbers do not contain any leading zero,
-    except the number 0 itself.
-*/
+
 public class AddNumbers {
 
+    /*
+        You are given two non-empty linked lists representing two non-negative integers.
+        The digits are stored in reverse order, and each of their nodes contains a single digit.
+        Add the two numbers and return the sum as a linked list.
+        You may assume the two numbers do not contain any leading zero,
+        except the number 0 itself.
+    */
     public ListNode addTwoNumbers(final ListNode l1, final ListNode l2) {
         return addTwoNumbers(l1, l2, 0);
+    }
+
+    /*
+        You are given two non-empty linked lists representing two non-negative integers.
+        The most significant digit comes first and each of their nodes contains a single digit.
+        Add the two numbers and return the sum as a linked list.
+        You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+     */
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        Stack<Integer> st1 = new Stack<>();
+        Stack<Integer> st2 = new Stack<>();
+        Stack<Integer> st3 = new Stack<>();
+        while (l1 != null) {
+            st1.push(l1.getVal());
+            l1 = l1.getNext();
+        }
+        while (l2 != null) {
+            st2.push(l2.getVal());
+            l2 = l2.getNext();
+        }
+        int carry = 0;
+        while (!st1.isEmpty() || !st2.isEmpty() || carry != 0) {
+            if (!st1.isEmpty()) {
+                carry += st1.pop();
+            }
+
+            if (!st2.isEmpty()) {
+                carry += st2.pop();
+            }
+
+            st3.push(carry % 10);
+            carry = carry / 10;
+        }
+        ListNode dummy = new ListNode();
+        ListNode itr = dummy;
+        while (!st3.isEmpty()) {
+            itr.setNext(new ListNode(st3.pop()));
+            itr = itr.getNext();
+        }
+        return dummy.getNext();
     }
 
     private ListNode addTwoNumbers(final ListNode l1, final ListNode l2, final int overflow) {
