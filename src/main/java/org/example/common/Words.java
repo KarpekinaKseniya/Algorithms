84a44e2b3445127dea7ac620adac3702e4b6c3e6
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-07-30 09:01:37 +0300

    ADD: simply solution for printer

--- src/main/java/org/example/common/Words.java
+++ src/main/java/org/example/common/Words.java
@@ -358,4 +358,31 @@ public class Words {
         }
         return false;
     }
+
+    /*
+        There is a strange printer with the following two special properties:
+            The printer can only print a sequence of the same character each time.
+            At each turn, the printer can print new characters starting from and ending
+                at any place and will cover the original existing characters.
+        Given a string s, return the minimum number of turns the printer needed to print it.
+    */
+    public int strangePrinter(final String s) {
+        final int n = s.length();
+        if (n == 0) return 0;
+
+        int[][] dp = new int[101][101];
+        for (int i = 0; i < n; i++) dp[i][i] = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                dp[j][j + i] = i + 1;
+                for (int k = j + 1; k <= j + i; k++) {
+                    int temp = dp[j][k - 1] + dp[k][j + i];
+                    if (s.charAt(k - 1) == s.charAt(j + i)) temp--;
+                    dp[j][j + i] = Math.min(dp[j][j + i], temp);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
 }
