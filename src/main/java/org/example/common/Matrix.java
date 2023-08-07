7774e215af78121676e8c76e0e02c60e9b58067b
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-08-07 07:34:17 +0300

    ADD: simply solution for search matrix

--- src/main/java/org/example/common/Matrix.java
+++ src/main/java/org/example/common/Matrix.java
@@ -96,4 +96,21 @@ public class Matrix {
         }
         return ans;
     }
+
+    /*
+        You are given an m x n integer matrix with the following two properties:
+            Each row is sorted in non-decreasing order.
+            The first integer of each row is greater than the last integer of the previous row.
+        Given an integer target, return true if target is in matrix or false otherwise.
+        You must write a solution in O(log(m * n)) time complexity.
+    */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (final int[] ints : matrix) {
+            for (final int anInt : ints) {
+                if (anInt == target)
+                    return true;
+            }
+        }
+        return false;
+    }
 }
