43f8632aefa14a00bd1ce0fa09e00044dac3a753
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-08-09 07:36:16 +0300

    ADD: simply solution for min max

--- src/main/java/org/example/common/WorkWithArrays.java
+++ src/main/java/org/example/common/WorkWithArrays.java
@@ -496,4 +496,43 @@ class WorkWithArrays {
     }
     return index;
   }
+
+  /*
+      You are given a 0-indexed integer array nums and an integer p.
+      Find p pairs of indices of nums such that the maximum difference amongst all the pairs
+      is minimized. Also, ensure no index appears more than once amongst the p pairs.
+      Note that for a pair of elements at the index i and j, the difference of this pair
+      is |nums[i] - nums[j]|, where |x| represents the absolute value of x.
+      Return the minimum maximum difference among all p pairs. We define the maximum
+      of an empty set to be zero.
+  */
+  public int minimizeMax(int[] nums, int p) {
+    if(nums.length == 1) {
+      return 0;
+    }
+    int ans = 0;
+    Arrays.sort(nums);
+    int low = 0,high = nums[nums.length - 1] - nums[0];
+    while(low <= high) {
+      int mid = (low + high) / 2;
+      int i = 1;
+      int count = 0;
+      while(i < nums.length) {
+        if(nums[i] - nums[i - 1] <= mid) {
+          count++;
+          i = i+2;
+        } else {
+          i++;
+        }
+      }
+      if(count >= p) {
+        ans = mid;
+        high = mid - 1;
+      }
+      else {
+        low = mid + 1;
+      }
+    }
+    return ans;
+  }
 }
