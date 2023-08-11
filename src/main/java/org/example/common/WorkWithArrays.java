22194b3aa7611bd4f2a50d110d9960d67e3f0150
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-08-11 06:38:11 +0300

    ADD: simply solution for coins

--- src/main/java/org/example/common/WorkWithArrays.java
+++ src/main/java/org/example/common/WorkWithArrays.java
@@ -554,4 +554,22 @@ class WorkWithArrays {
         return true;
     return false;
   }
+
+  /*
+      You are given an integer array coins representing coins of different denominations
+      and an integer amount representing a total amount of money.
+      Return the number of combinations that make up that amount. If that amount of money
+      cannot be made up by any combination of the coins, return 0.
+      You may assume that you have an infinite number of each kind of coin.
+      The answer is guaranteed to fit into a signed 32-bit integer.
+  */
+  public int change(int amount, int[] coins) {
+    int arr[] = new int[amount+1];
+    arr[0] = 1;
+    for(final int coin : coins)
+      for(int i = coin; i <= amount; i++)
+        arr[i] += arr[i-coin];
+    return arr[amount];
+  }
+
 }
