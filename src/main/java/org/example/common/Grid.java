6797477ac3ddf7871ce69f8b28d57c1a8ecd3a10
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-08-12 09:02:07 +0300

    ADD: simply solution for unique path

--- src/main/java/org/example/common/Grid.java
+++ src/main/java/org/example/common/Grid.java
@@ -94,6 +94,52 @@ public class Grid {
     }
 
     /*
+         You are given an m x n integer array grid. There is a robot initially located at
+         the top-left corner (i.e., grid[0][0]). The robot tries to move to the
+         bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either
+         down or right at any point in time.
+         An obstacle and space are marked as 1 or 0 respectively in grid. A path that
+         the robot takes cannot include any square that is an obstacle.
+         Return the number of possible unique paths that the robot can take to reach
+         the bottom-right corner.
+         The testcases are generated so that the answer will be less than or equal to 2 * 10^9.
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0)
+            return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
+            return 0;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                dp[i][0] = 0;
+            } else {
+                dp[i][0] = dp[i - 1][0];
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                dp[0][i] = 0;
+            } else {
+                dp[0][i] = dp[0][i - 1];
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /*
         You are given an m x n integer matrix grid, where you can move from a cell to
         any adjacent cell in all 4 directions.
         Return the number of strictly increasing paths in the grid such that you can
