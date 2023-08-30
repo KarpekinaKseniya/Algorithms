0ac659eaf1245d84c3a6c38e3cade2478f2a4467
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-08-30 07:27:02 +0300

    ADD: simply solution for find min replacement

--- src/main/java/org/example/common/WorkWithArrays.java
+++ src/main/java/org/example/common/WorkWithArrays.java
@@ -587,4 +587,28 @@ class WorkWithArrays {
     }
     return p.peek();
   }
+
+  /*
+      You are given a 0-indexed integer array nums. In one operation you can replace any element
+      of the array with any two elements that sum to it.
+      For example, consider nums = [5,6,7]. In one operation, we can replace nums[1] with 2 and 4 and
+      convert nums to [5,2,4,7].
+      Return the minimum number of operations to make an array that is sorted in non-decreasing order.
+   */
+  public long minimumReplacement(int[] nums) {
+     long result = 0;
+     int n = nums.length;
+     int prev = nums[n - 1];
+     for(int i = n - 2; i >= 0; i--){
+         if(prev >= nums[i]){
+             prev = nums[i];
+         }
+         else{
+             int parts = (int)Math.ceil((double)nums[i] / prev) - 1;
+             result += parts;
+             prev = Math.min(prev, nums[i] / (parts + 1));
+            }
+     }
+     return result;
+    }
 }
