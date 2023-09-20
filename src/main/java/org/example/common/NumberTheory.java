c371b46c8c55caf93903037e992153ffb37a55f7
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-09-20 07:40:35 +0300

    ADD: simply solution for reduce operations

--- src/main/java/org/example/common/NumberTheory.java
+++ src/main/java/org/example/common/NumberTheory.java
@@ -208,4 +208,23 @@ public class NumberTheory {
         }
         return nums.length;
     }
+
+    /*
+        You are given an integer array nums and an integer x. In one operation, you can either remove
+        the leftmost or the rightmost element from the array nums and subtract its value from x.
+        Note that this modifies the array for future operations.
+        Return the minimum number of operations to reduce x to exactly 0 if it is possible, otherwise,
+        return -1.
+    */
+    public int minOperations(int[] nums, int x) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+        int maxLength = -1, currSum = 0;
+        for (int l = 0, r = 0; r < nums.length; r++) {
+            currSum += nums[r];
+            while (l <= r && currSum > sum - x) currSum -= nums[l++];
+            if (currSum == sum - x) maxLength = Math.max(maxLength, r - l + 1);
+        }
+        return maxLength == -1 ? -1 : nums.length - maxLength;
+    }
 }
