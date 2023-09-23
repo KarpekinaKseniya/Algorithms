86d607275a1daf3cc9c1ac6b4f0b9143dba2fabb
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-09-23 08:31:02 +0300

    ADD: simply solution for find the longest chain

--- src/main/java/org/example/common/Words.java
+++ src/main/java/org/example/common/Words.java
@@ -523,4 +523,35 @@ public class Words {
         }
         return dp[s1.length()][s2.length()];
     }
+
+    /*
+        You are given an array of words where each word consists of lowercase English letters.
+        wordA is a predecessor of wordB if and only if we can insert exactly one letter anywhere
+        in wordA without changing the order of the other characters to make it equal to wordB.
+        For example, "abc" is a predecessor of "abac", while "cba" is not a predecessor of "bcad".
+        A word chain is a sequence of words [word1, word2, ..., wordk] with k >= 1, where word1
+        is a predecessor of word2, word2 is a predecessor of word3, and so on. A single word
+        is trivially a word chain with k == 1.
+        Return the length of the longest possible word chain with words chosen from the given
+        list of words.
+    */
+    public int longestStrChain(String[] words) {
+        if (words == null || words.length == 0) {
+            return 0;
+        }
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        Map<String, Integer> map = new HashMap<>();
+        int max = 0;
+        for (String word : words) {
+            int longestSubstring = 0;
+            for (int i = 0; i < word.length(); i++) {
+                String subWord = word.substring(0, i) + word.substring(i + 1);
+                longestSubstring = Math.max(longestSubstring, map.getOrDefault(subWord, 0) + 1);
+            }
+            map.put(word, longestSubstring);
+            max = Math.max(max, longestSubstring);
+        }
+        return max;
+    }
+
 }
