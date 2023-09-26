fd597480ae6614b7883f7de5839392b97b66454e
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-09-26 08:02:54 +0300

    ADD: simply solution for removing duplicate letters

--- src/main/java/org/example/common/Words.java
+++ src/main/java/org/example/common/Words.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 
 public class Words {
 
@@ -570,5 +571,39 @@ public class Words {
             sum2 = sum2 + ch;
         }
         return (char) (sum2 - sum1);
+    }
+
+    /*
+        Given a string s, remove duplicate letters so that every letter appears once and only once.
+        You must make sure your result is the smallest in lexicographical order among all possible
+        results.
+     */
+    public String removeDuplicateLetters(String s) {
+        HashMap<Character, Integer> inStack = new HashMap<>();
+        HashMap<Character, Integer> freq = new HashMap<>();
+        Stack<Character> stk = new Stack<>();
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            if (inStack.containsKey(ch)) {
+                freq.put(ch, freq.getOrDefault(ch, 0) - 1);
+                continue;
+            }
+            while (stk.size() > 0 && stk.peek() > ch && freq.get(stk.peek()) >= 1) {
+                inStack.remove(stk.peek());
+                stk.pop();
+            }
+            stk.push(ch);
+            inStack.put(ch, 1);
+            freq.put(ch, freq.getOrDefault(ch, 0) - 1);
+        }
+        StringBuilder res = new StringBuilder();
+        while (stk.size() > 0) {
+            res.insert(0, stk.pop());
+        }
+        return res.toString();
     }
 }
