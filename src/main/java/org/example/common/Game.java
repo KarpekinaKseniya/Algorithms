07a05cddd19c902fb920d9d39913766224e12752
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-10-02 07:28:39 +0300

    ADD: simply solution for colors game

--- src/main/java/org/example/common/Game.java
+++ src/main/java/org/example/common/Game.java
@@ -1,16 +1,16 @@
 package org.example.common;
 
-/*
-    Alice plays the following game, loosely based on the card game "21".
-    Alice starts with 0 points and draws numbers while she has less than k points.
-    During each draw, she gains an integer number of points randomly from the range [1, maxPts],
-    where maxPts is an integer. Each draw is independent and the outcomes have equal probabilities.
-    Alice stops drawing numbers when she gets k or more points.
-    Return the probability that Alice has n or fewer points.
-    Answers within 10-5 of the actual answer are considered accepted.
-*/
 public class Game {
 
+    /*
+        Alice plays the following game, loosely based on the card game "21".
+        Alice starts with 0 points and draws numbers while she has less than k points.
+        During each draw, she gains an integer number of points randomly from the range [1, maxPts],
+        where maxPts is an integer. Each draw is independent and the outcomes have equal probabilities.
+        Alice stops drawing numbers when she gets k or more points.
+        Return the probability that Alice has n or fewer points.
+        Answers within 10-5 of the actual answer are considered accepted.
+    */
     public double new21Game(final int n, final int k, final int maxPts) {
         if (k == 0 || n >= k + maxPts) {
             return 1.0;
@@ -34,4 +34,21 @@ public class Game {
         return probability;
     }
 
+    /*
+        There are n pieces arranged in a line, and each piece is colored either by 'A' or by 'B'. You are
+         given a string colors of length n where colors[i] is the color of the ith piece.
+        Alice and Bob are playing a game where they take alternating turns removing pieces from the line.
+        In this game, Alice moves first.
+        Alice is only allowed to remove a piece colored 'A' if both its neighbors are also colored 'A'.
+        She is not allowed to remove pieces that are colored 'B'.
+        Bob is only allowed to remove a piece colored 'B' if both its neighbors are also colored 'B'.
+        He is not allowed to remove pieces that are colored 'A'.
+        Alice and Bob cannot remove pieces from the edge of the line.
+        If a player cannot make a move on their turn, that player loses and the other player wins.
+        Assuming Alice and Bob play optimally, return true if Alice wins, or return false if Bob wins.
+    */
+    public boolean winnerOfGame(String colors) {
+        return colors.replaceAll("A{3,}", "AA").length() <
+                colors.replaceAll("B{3,}", "BB").length();
+    }
 }
