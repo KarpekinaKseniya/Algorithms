b4a902120d5b245b6fba7f89370dfa15ed9ac214
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-10-03 08:24:52 +0300

    ADD: simply solution for number of good pairs

--- src/main/java/org/example/common/NumberTheory.java
+++ src/main/java/org/example/common/NumberTheory.java
@@ -227,4 +227,20 @@ public class NumberTheory {
         }
         return maxLength == -1 ? -1 : nums.length - maxLength;
     }
+
+    /*
+        Given an array of integers nums, return the number of good pairs.
+        A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+    */
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
