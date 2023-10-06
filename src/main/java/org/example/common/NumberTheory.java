54b796cda95b8e7dbe9faab3c83a24fe61be67a8
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-10-06 08:28:53 +0300

    ADD: simply solution for find max product

--- src/main/java/org/example/common/NumberTheory.java
+++ src/main/java/org/example/common/NumberTheory.java
@@ -243,4 +243,23 @@ public class NumberTheory {
         }
         return count;
     }
+
+    /*
+        Given an integer n, break it into the sum of k positive integers, where k >= 2,
+        and maximize the product of those integers.
+        Return the maximum product you can get.
+    */
+    public int integerBreak(int n) {
+        if (n == 2)
+            return 1;
+        if (n == 3)
+            return 2;
+        int prod = 1;
+        while (n > 4) {
+            prod *= 3;
+            n -= 3;
+        }
+        prod *= n;
+        return prod;
+    }
 }
