593cb00bfd7276ef838c48acfc8c7b66ce4c81a9
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-10-23 08:33:37 +0300

    ADD: simply solution for powers of four

--- src/main/java/org/example/common/NumberTheory.java
+++ src/main/java/org/example/common/NumberTheory.java
@@ -262,4 +262,21 @@ public class NumberTheory {
         prod *= n;
         return prod;
     }
+
+    /*
+        Given an integer n, return true if it is a power of four. Otherwise, return false.
+        An integer n is a power of four, if there exists an integer x such that n == 4x.
+    */
+    public boolean isPowerOfFour(int n) {
+        if (n == 1)
+            return true;
+        if (n < 4 || n % 4 != 0)
+            return false;
+        while (n != 4) {
+            if (n % 4 != 0)
+                return false;
+            n /= 4;
+        }
+        return true;
+    }
 }
