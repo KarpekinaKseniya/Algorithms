bd7581b3945a5f323e8549740fbbba767db65264
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-10-25 07:54:05 +0300

    ADD: simply solution for k-th symbol

--- src/main/java/org/example/common/Arithmetic.java
+++ src/main/java/org/example/common/Arithmetic.java
@@ -23,4 +23,22 @@ public class Arithmetic {
         return max + 1;
     }
 
+    /*
+        We build a table of n rows (1-indexed). We start by writing 0 in the 1st row.
+        Now in every subsequent row, we look at the previous row and replace each occurrence
+        of 0 with 01, and each occurrence of 1 with 10.
+        For example, for n = 3, the 1st row is 0, the 2nd row is 01, and the 3rd row is 0110.
+        Given two integer n and k, return the kth (1-indexed) symbol in the nth row of a table
+        of n rows.
+    */
+    public int kthGrammar(int n, int k) {
+        if (n == 1 && k == 1)
+            return 0;
+        int size = (int) Math.pow(2, n - 2);
+        if (k <= size) {
+            return kthGrammar(n - 1, k);
+        } else {
+            return kthGrammar(n - 1, k - size) ^ 1;
+        }
+    }
 }
