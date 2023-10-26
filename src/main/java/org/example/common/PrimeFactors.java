24880196e789dbe46ec51dfd2d160704500019b1
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-10-26 08:35:38 +0300

    ADD: simply solution for num factored

--- src/main/java/org/example/common/PrimeFactors.java
+++ src/main/java/org/example/common/PrimeFactors.java
@@ -1,13 +1,16 @@
 package org.example.common;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-/*
-    Find all prime factors of a given number
- */
 class PrimeFactors {
 
+    /*
+        Find all prime factors of a given number
+    */
     public int[] findAllPrimeFactors(final int n) {
         int number = n;
         final List<Integer> primes = new ArrayList<>();
@@ -26,4 +29,29 @@ class PrimeFactors {
         }
         return primes.stream().mapToInt(Integer::intValue).toArray();
     }
+
+    /*
+        Given an array of unique integers, arr, where each integer arr[i] is strictly greater than 1.
+        We make a binary tree using these integers, and each number may be used for any number of times.
+        Each non-leaf node's value should be equal to the product of the values of its children.
+        Return the number of binary trees we can make. The answer may be too large so return the answer
+        modulo 10^9 + 7.
+    */
+    public int numFactoredBinaryTrees(int[] arr) {
+        Arrays.sort(arr);
+        Map<Integer, Long> map = new HashMap<>();
+        long ans = 1;
+        map.put(arr[0], ans);
+        for (int i = 1; i < arr.length; i++) {
+            long sum = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] % arr[j] == 0 && map.containsKey(arr[i] / arr[j]))
+                    sum = sum + (map.get(arr[i] / arr[j]) * map.get(arr[j]));
+            }
+            map.put(arr[i], sum);
+            ans = ans + sum;
+        }
+        return (int) (ans % 1_000_000_007);
+    }
+
 }
