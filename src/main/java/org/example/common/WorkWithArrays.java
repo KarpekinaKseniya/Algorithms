79c791508583a04019f6806ee12f3ea06ea05146
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-11-15 07:56:50 +0300

    ADD: simply solution for max element

--- src/main/java/org/example/common/WorkWithArrays.java
+++ src/main/java/org/example/common/WorkWithArrays.java
@@ -832,4 +832,24 @@ class WorkWithArrays {
     }
     return ans;
   }
+
+  /*
+      You are given an array of positive integers arr. Perform some operations (possibly none) on arr
+        so that it satisfies these conditions:
+          The value of the first element in arr must be 1.
+          The absolute difference between any 2 adjacent elements must be less than or equal to 1.
+          In other words, abs(arr[i] - arr[i - 1]) <= 1 for each i where 1 <= i < arr.length (0-indexed). abs(x) is the absolute value of x.
+      There are 2 types of operations that you can perform any number of times:
+          Decrease the value of any element of arr to a smaller positive integer.
+          Rearrange the elements of arr to be in any order.
+      Return the maximum possible value of an element in arr after performing the operations to satisfy
+      the conditions.
+  */
+  public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+    Arrays.sort(arr);
+    int last = 1;
+    for (int i = 1; i < arr.length; i++)
+      last = Math.min(arr[i], last + 1);
+    return last;
+  }
 }
