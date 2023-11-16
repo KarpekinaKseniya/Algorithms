c3a550210c77bf7342d98764b8f2abfec8946a7c
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-11-16 08:42:42 +0300

    ADD: simply solution for find unique binary string

--- src/main/java/org/example/common/Words.java
+++ src/main/java/org/example/common/Words.java
@@ -719,6 +719,36 @@ public class Words {
     }
 
     /*
+        Given an array of strings nums containing n unique binary strings each of length n, return
+        a binary string of length n that does not appear in nums. If there are multiple answers,
+        you may return any of them.
+    */
+    public String findDifferentBinaryString(String[] nums) {
+        int[] arr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) arr[i] = Integer.parseInt(nums[i], 2);
+        Arrays.sort(arr);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] > 1) {
+                String res = Integer.toBinaryString(arr[i] - 1);
+                StringBuilder sb = new StringBuilder();
+                while (sb.length() + res.length() < nums[0].length()) sb.append("0");
+                sb.append(res);
+                return sb.toString();
+            }
+        }
+        if (arr[0] != 0) {
+            StringBuilder sb = new StringBuilder();
+            while (sb.length() < nums[0].length()) sb.append("0");
+            return sb.toString();
+        }
+        String res = Integer.toBinaryString(arr[arr.length - 1] + 1);
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() + res.length() < nums[0].length()) sb.append("0");
+        sb.append(res);
+        return sb.toString();
+    }
+
+    /*
         Given a string s, reverse the order of characters in each word within a sentence while still
         preserving whitespace and initial word order.
     */
