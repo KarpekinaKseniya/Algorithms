e699cde1c287f76806438c05d240cc9db7666c5b
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-11-17 07:49:19 +0300

    ADD: simply solution for find array pair sum

--- src/main/java/org/example/common/WorkWithArrays.java
+++ src/main/java/org/example/common/WorkWithArrays.java
@@ -852,4 +852,25 @@ class WorkWithArrays {
       last = Math.min(arr[i], last + 1);
     return last;
   }
+
+  /*
+    The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in
+    a list of pairs.
+    For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair sum would be max(1+5, 2+3,
+    4+4) = max(6, 5, 8) = 8.
+    Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+      Each element of nums is in exactly one pair.
+      The maximum pair sum is minimized.
+    Return the minimized maximum pair sum after optimally pairing up the elements.
+  */
+  public int minPairSum(final int[] nums) {
+    Arrays.sort(nums);
+    int max = Integer.MIN_VALUE;
+    for(int i = 0; i < nums.length / 2; i++){
+      final int sum = nums[i] + nums[nums.length - i - 1];
+      max = Math.max(max, sum);
+    }
+    return max;
+  }
+
 }
