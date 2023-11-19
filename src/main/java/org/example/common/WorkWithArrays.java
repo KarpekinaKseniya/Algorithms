ee57620e954b3d628eca8db985797450a98e7434
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-11-19 09:06:32 +0300

    ADD: simply solution for reduction

--- src/main/java/org/example/common/WorkWithArrays.java
+++ src/main/java/org/example/common/WorkWithArrays.java
@@ -902,4 +902,27 @@ class WorkWithArrays {
     return maxFrequency;
   }
 
+  /*
+      Given an integer array nums, your goal is to make all elements in nums equal.
+      To complete one operation, follow these steps:
+        Find the largest value in nums. Let its index be i (0-indexed) and its value be
+        largest. If there are multiple elements with the largest value, pick the smallest i.
+        Find the next largest value in nums strictly smaller than largest. Let its value be
+        nextLargest.
+        Reduce nums[i] to nextLargest.
+      Return the number of operations to make all elements in nums equal.
+   */
+  public int reductionOperations(int[] nums) {
+    Arrays.sort(nums);
+    int res = 0;
+    int curr = 0;
+    for(int i = 1; i < nums.length; i++){
+      if(nums[i] > nums[i - 1]){
+        curr++;
+      }
+      res = res + curr;
+    }
+    return res;
+  }
+
 }
