658c3452fa660fa3fb7b25efea17c0205681433e
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-11-23 08:59:29 +0300

    ADD: simply solution for subarrays

--- src/main/java/org/example/common/WorkWithArrays.java
+++ src/main/java/org/example/common/WorkWithArrays.java
@@ -958,4 +958,48 @@ class WorkWithArrays {
     return res;
   }
 
+  /*
+      A sequence of numbers is called arithmetic if it consists of at least two elements, and
+      the difference between every two consecutive elements is the same. More formally,
+      a sequence s is arithmetic if and only if s[i+1] - s[i] == s[1] - s[0] for all valid i.
+      For example, these are arithmetic sequences:
+        1, 3, 5, 7, 9
+        7, 7, 7, 7
+        3, -1, -5, -9
+      The following sequence is not arithmetic:
+        1, 1, 2, 5, 7
+      You are given an array of n integers, nums, and two arrays of m integers each, l and r,
+      representing the m range queries, where the ith query is the range [l[i], r[i]]. All the arrays
+      are 0-indexed.
+      Return a list of boolean elements answer, where answer[i] is true if the subarray nums[l[i]],
+      nums[l[i]+1], ... , nums[r[i]] can be rearranged to form an arithmetic sequence, and false
+      otherwise.
+  */
+  public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+    int n = l.length;
+    List<Boolean> ans = new ArrayList<>(n);
+    for(int i = 0; i < n; i++){
+      int len = r[i] - l[i] + 1;
+      if(len > 1){
+        int[] temp = Arrays.copyOfRange(nums, l[i], r[i] + 1);
+        Arrays.sort(temp);
+        int req = temp[1] - temp[0];
+        boolean olredi = false;
+        for(int j = 0; j < len-1; j++){
+          if(temp[j + 1] - temp[j] != req){
+            ans.add(false);
+            olredi = true;
+            break;
+          }
+        }
+        if(!olredi){
+          ans.add(true);
+        }
+      }else{
+        ans.add(false);
+      }
+    }
+    return ans;
+  }
+
 }
