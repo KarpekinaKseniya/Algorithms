a4020c54d0bd75a805d87ab5f4da766a8bac2fd9
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-11-24 09:29:12 +0300

    ADD: simply solution for max coins

--- src/main/java/org/example/common/Cost.java
+++ src/main/java/org/example/common/Cost.java
@@ -53,4 +53,42 @@ public class Cost {
         }
         return (int) dp[dp.length - 1];
     }
+
+    /*
+        There are 3n piles of coins of varying size, you and your friends will take piles of coins
+        as follows:
+            In each step, you will choose any 3 piles of coins (not necessarily consecutive).
+            Of your choice, Alice will pick the pile with the maximum number of coins.
+            You will pick the next pile with the maximum number of coins.
+            Your friend Bob will pick the last pile.
+            Repeat until there are no more piles of coins.
+            Given an array of integers piles where piles[i] is the number of coins in the ith pile.
+        Return the maximum number of coins that you can have.
+    */
+    public int maxCoins(int[] piles) {
+        int maxCoins = 0;
+        int[] freq = new int[100001];
+        for (int i : piles) {
+            freq[i]++;
+        }
+        int chances = piles.length / 3;
+        int turn = 1;
+        int i = freq.length - 1;
+        while (chances != 0) {
+            if (freq[i] > 0) {
+                if (turn == 1) {
+                    turn = 0;
+                } else {
+                    maxCoins += i;
+                    turn = 1;
+                    chances--;
+                }
+
+                freq[i]--;
+            } else {
+                i--;
+            }
+        }
+        return maxCoins;
+    }
 }
