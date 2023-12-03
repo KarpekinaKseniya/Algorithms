ea23616232b76de4acfaba682ea41b8168b72099
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-12-03 10:19:18 +0300

    ADD: simply solution for points

--- src/main/java/org/example/common/PointsOnLine.java
+++ src/main/java/org/example/common/PointsOnLine.java
@@ -70,4 +70,28 @@ public class PointsOnLine {
         }
         return res;
     }
+
+    /*
+        On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi].
+        Return the minimum time in seconds to visit all the points in the order given by points.
+        You can move according to these rules:
+            In 1 second, you can either:
+                move vertically by one unit,
+                move horizontally by one unit, or
+                move diagonally sqrt(2) units (in other words, move one unit vertically then
+                    one unit horizontally in 1 second).
+            You have to visit the points in the same order as they appear in the array.
+            You are allowed to pass through points that appear later in the order, but these
+                do not count as visits.
+    */
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int steps = 0;
+        for (int i = 0; i < points.length - 1; i++) {
+            int x = Math.abs(points[i][0] - points[i + 1][0]);
+            int y = Math.abs(points[i][1] - points[i + 1][1]);
+            steps += Math.max(x, y);
+        }
+        return steps;
+    }
+
 }
