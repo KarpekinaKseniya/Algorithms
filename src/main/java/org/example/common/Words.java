34cd18afe3025da1bae9d7e9c997884360c1d17c
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-12-04 09:31:58 +0300

    ADD: simply solution for good number

--- src/main/java/org/example/common/Words.java
+++ src/main/java/org/example/common/Words.java
@@ -749,6 +749,24 @@ public class Words {
     }
 
     /*
+        You are given a string num representing a large integer. An integer is good if it meets the
+        following conditions:
+            It is a substring of num with length 3.
+            It consists of only one unique digit.
+        Return the maximum good integer as a string or an empty string "" if no such integer exists.
+        Note:
+            A substring is a contiguous sequence of characters within a string.
+            There may be leading zeroes in num or a good integer.
+    */
+    public String largestGoodInteger(String num) {
+        for (var i = 9; i >= 0; i--) {
+            var sub = String.format("%d%d%d", i, i, i);
+            if (num.contains(sub)) return sub;
+        }
+        return "";
+    }
+
+    /*
         Given a string s, reverse the order of characters in each word within a sentence while still
         preserving whitespace and initial word order.
     */
