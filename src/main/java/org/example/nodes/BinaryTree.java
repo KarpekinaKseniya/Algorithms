eb0178088b07298fec2dca8de9c46d9f5552568b
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-12-08 08:19:24 +0300

    ADD: simply solution for convert binary tree to string

--- src/main/java/org/example/nodes/BinaryTree.java
+++ src/main/java/org/example/nodes/BinaryTree.java
@@ -74,4 +74,20 @@ public class BinaryTree {
         return result;
     }
 
+    /*
+        Given the root of a binary tree, construct a string consisting of parenthesis and integers
+        from a binary tree with the preorder traversal way, and return it.
+        Omit all the empty parenthesis pairs that do not affect the one-to-one mapping relationship
+        between the string and the original binary tree.
+    */
+    public String tree2str(TreeNode root) {
+        if (root == null) return "";
+        var s = Integer.toString(root.getVal());
+        if (root.getLeft() == null && root.getRight() == null)
+            return s;
+        s += "(" + tree2str(root.getLeft()) + ")";
+        if (root.getRight() != null)
+            s += "(" + tree2str(root.getRight()) + ")";
+        return s;
+    }
 }
