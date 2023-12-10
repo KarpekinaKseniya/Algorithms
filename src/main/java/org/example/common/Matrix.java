0a8b43bc0d70a5c0b9d07d2a4637e7855fba5786
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-12-10 12:23:27 +0300

    ADD: simply solution for 2D matrix

--- src/main/java/org/example/common/Matrix.java
+++ src/main/java/org/example/common/Matrix.java
@@ -178,4 +178,21 @@ public class Matrix {
         }
         return weakestRows;
     }
+
+    /*
+        Given a 2D integer array matrix, return the transpose of matrix.
+        The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's
+        row and column indices.
+    */
+    public int[][] transpose(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] arr = new int[col][row];
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                arr[i][j] = matrix[j][i];
+            }
+        }
+        return arr;
+    }
 }
