58d10da308253be9e8b2cb18d5877d6ca5a068e7
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-12-11 08:58:19 +0300

    ADD: simply solution for array

--- src/main/java/org/example/common/WorkWithArrays.java
+++ src/main/java/org/example/common/WorkWithArrays.java
@@ -1026,4 +1026,19 @@ class WorkWithArrays {
     }
     return ans;
   }
+
+  /*
+      Given an integer array sorted in non-decreasing order, there is exactly one integer in the array
+      that occurs more than 25% of the time, return that integer.
+  */
+  public int findSpecialInteger(int[] arr) {
+    int n = arr.length;
+    int quarter = n / 4;
+    for (int i = 0; i < n - quarter; i++) {
+      if (arr[i] == arr[i + quarter]) {
+        return arr[i];
+      }
+    }
+    return -1;
+  }
 }
