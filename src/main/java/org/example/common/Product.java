caac9754952d2c742f9734734c0e3dcb4325f2cb
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-12-12 07:33:51 +0300

    ADD: simply solution for find max value

--- src/main/java/org/example/common/Product.java
+++ src/main/java/org/example/common/Product.java
@@ -48,4 +48,21 @@ public class Product {
         }
         return i;
     }
+
+    /*
+        Given the array of integers nums, you will choose two different indices i and j of that array.
+        Return the maximum value of (nums[i]-1)*(nums[j]-1).
+    */
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int mul = (nums[i] - 1) * (nums[j] - 1);
+                if (max < mul) {
+                    max = mul;
+                }
+            }
+        }
+        return max;
+    }
 }
