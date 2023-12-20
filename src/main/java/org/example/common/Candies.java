61a86e7c3a2edcf3877183d8230529294a325387
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-12-20 08:22:42 +0300

    ADD: simply solution for buy two chocolates

--- src/main/java/org/example/common/Candies.java
+++ src/main/java/org/example/common/Candies.java
@@ -65,4 +65,29 @@ public class Candies {
         }
         return sum;
     }
+
+    /*
+        You are given an integer array prices representing the prices of various chocolates in a store.
+        You are also given a single integer money, which represents your initial amount of money.
+        You must buy exactly two chocolates in such a way that you still have some non-negative leftover
+        money. You would like to minimize the sum of the prices of the two chocolates you buy.
+        Return the amount of money you will have leftover after buying the two chocolates. If there is no
+        way for you to buy two chocolates without ending up in debt, return money. Note that the leftover
+        must be non-negative.
+    */
+    public int buyChoco(int[] prices, int money) {
+        var min1 = 101;
+        var min2 = 101;
+        for (var price : prices) {
+            if (price < min1) {
+                min2 = min1;
+                min1 = price;
+            } else if (price < min2) {
+                min2 = price;
+            }
+        }
+        var diff = money - min1 - min2;
+        return diff >= 0 ? diff : money;
+    }
+
 }
