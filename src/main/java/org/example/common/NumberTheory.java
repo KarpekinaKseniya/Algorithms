cf2a31c3882923fb280e941e320eff2de30059c9
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-12-26 10:11:19 +0300

    ADD: simply solution for rolls num

--- src/main/java/org/example/common/NumberTheory.java
+++ src/main/java/org/example/common/NumberTheory.java
@@ -321,4 +321,25 @@ public class NumberTheory {
         }
         return "";
     }
+
+    /*
+        You have n dice, and each die has k faces numbered from 1 to k.
+        Given three integers n, k, and target, return the number of possible ways (out of the kn total
+        ways) to roll the dice, so the sum of the face-up numbers equals target. Since the answer may be
+        too large, return it modulo 10^9 + 7.
+    */
+    public int numRollsToTarget(int n, int k, int target) {
+        var dp = new int[n][target];
+        for (var i = 0; i < Math.min(k, target); i++) {
+            dp[n - 1][i] = 1;
+        }
+        for (var i = n - 2; i >= 0; i--) {
+            for (var j = 0; j < target; j++) {
+                for (var x = 0; x < Math.min(k, j); x++) {
+                    dp[i][j] = (dp[i][j] + dp[i + 1][j - x - 1]) % 1000000007;
+                }
+            }
+        }
+        return dp[0][target - 1];
+    }
 }
