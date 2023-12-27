8e7ec287f1df76fc87ac2cd1f0ebfc94e0fd8ede
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-12-27 14:49:06 +0300

    ADD: simply solution for min cost of balloons

--- src/main/java/org/example/common/Cost.java
+++ src/main/java/org/example/common/Cost.java
@@ -91,4 +91,26 @@ public class Cost {
         }
         return maxCoins;
     }
+
+    /*
+        Alice has n balloons arranged on a rope. You are given a 0-indexed string colors where
+        colors[i] is the color of the ith balloon.
+        Alice wants the rope to be colorful. She does not want two consecutive balloons to be of
+        the same color, so she asks Bob for help. Bob can remove some balloons from the rope to make
+        it colorful. You are given a 0-indexed integer array neededTime where neededTime[i] is
+        the time (in seconds) that Bob needs to remove the ith balloon from the rope.
+        Return the minimum time Bob needs to make the rope colorful.
+    */
+    public int minCost(String colors, int[] neededTime) {
+        int max = 0, res = 0, n = neededTime.length;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && colors.charAt(i) == colors.charAt(i - 1)) {
+                res += Math.min(max, neededTime[i]);
+                max = Math.max(max, neededTime[i]);
+            } else {
+                max = neededTime[i];
+            }
+        }
+        return res;
+    }
 }
