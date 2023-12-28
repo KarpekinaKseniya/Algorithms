974efcad7a2c0182cf6d6e9a705b53f7cf3138af
KarpekinaKseniya <feliks.cat37@gmail.com>
2023-12-28 08:55:16 +0300

    ADD: simply solution for optimal compression

--- src/main/java/org/example/common/Words.java
+++ src/main/java/org/example/common/Words.java
@@ -767,6 +767,49 @@ public class Words {
     }
 
     /*
+        Run-length encoding is a string compression method that works by replacing consecutive identical
+        characters (repeated 2 or more times) with the concatenation of the character and the number
+        marking the count of the characters (length of the run). For example, to compress the string
+        "aabccc" we replace "aa" by "a2" and replace "ccc" by "c3". Thus the compressed string becomes
+        "a2bc3".
+        Notice that in this problem, we are not adding '1' after single characters.
+        Given a string s and an integer k. You need to delete at most k characters from s such that
+        the run-length encoded version of s has minimum length.
+        Find the minimum length of the run-length encoded version of s after deleting at most
+        k characters.
+    */
+    public int getLengthOfOptimalCompression(String s, int k) {
+        int n = s.length();
+        int[][] dp = new int[110][110];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                dp[i][j] = 9999;
+            }
+        }
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                int cnt = 0, del = 0;
+                for (int l = i; l >= 1; l--) {
+                    if (s.charAt(l - 1) == s.charAt(i - 1)) {
+                        cnt++;
+                    } else {
+                        del++;
+                    }
+                    if (j - del >= 0) {
+                        int len = cnt >= 100 ? 3 : cnt >= 10 ? 2 : cnt >= 2 ? 1 : 0;
+                        dp[i][j] = Math.min(dp[i][j], dp[l - 1][j - del] + 1 + len);
+                    }
+                }
+                if (j > 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1]);
+                }
+            }
+        }
+        return dp[n][k];
+    }
+
+    /*
         Given a string s, reverse the order of characters in each word within a sentence while still
         preserving whitespace and initial word order.
     */
